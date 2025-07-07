6add4560d8c65b3feefb999a1787d93b0a3a34de
golshid <golsheedd@gmail.com>
2025-07-07 09:13:18 +0330

    add responsive tHead to project

---
+++ src/components/tableHead/TableHead.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./TableHead.css";
+import { useState, useEffect } from "react";
+import SwapVertIcon from "@mui/icons-material/SwapVert";
+
+export default function TableHead({ props, onSortByFirst, onSortByLast }) {
+  function sortByFirstClicked() {
+    onSortByFirst();
+  }
+
+  function sortByLastClicked() {
+    onSortByLast();
+  }
+  
+  return (
+    <table className="TableHead-container">
+      <tr className="TableHead-row">
+        {props.map((obj) => (
+          <th className="TableHead-cell">{obj.title}</th>
+        ))}
+        <th className="TableHead-cell TableHead-sort-icon-wrapper">
+          <SwapVertIcon className="TableHead-sort-icon" />
+          <div className="TableHead-sort-icon-sub-menu">
+            <div
+              className="TableHead-sort-icon-sub-menu-item"
+              onClick={sortByFirstClicked}
+            >
+              sort by first
+            </div>
+            <div
+              className="TableHead-sort-icon-sub-menu-item"
+              onClick={sortByLastClicked}
+            >
+              sort by last
+            </div>
+          </div>
+        </th>
+      </tr>
+    </table>
+  );
+}
--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,6 +1,32 @@
 import React from "react";
 import "./Users.css";
+import TableHead from "../../components/tableHead/TableHead";
+import { useState } from "react";
 
 export default function Users() {
-  return <div>Users</div>;
+  const [tHeadData, setTHeadData] = useState([
+    { id: 1, title: "Name" },
+    { id: 2, title: "Family Name" },
+    { id: 3, title: "Email" },
+    { id: 4, title: "Phone" },
+    { id: 5, title: "Address" },
+  ]);
+
+  function sortByFirst() {
+    console.log("sort by first");
+  }
+
+  function sortByLast() {
+    console.log("sort by last");
+  }
+
+  return (
+    <>
+      <TableHead
+        props={tHeadData}
+        onSortByFirst={sortByFirst}
+        onSortByLast={sortByLast}
+      />
+    </>
+  );
 }
